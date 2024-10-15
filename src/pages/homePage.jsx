cf3f150d87ce3aeb2c4663ad46babec96b46decc
Thomas Martin <161907136+TommyMart@users.noreply.github.com>
2024-10-15 18:32:05 +1030

    implemented user context update.

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useUser } from "../contexts/UserContext";
 
 function HomePage() {
 
     // Get id from the URL
     const { id } = useParams();
 
-    return (
-        <h1>Welcome to your home page, {user.name}.</h1>
-    )
+    const { user } = useUser();
+
+    <div>
+            {user ? (
+                <h1>Welcome to your home page, {user.name}.</h1>
+            ) : (
+                <h1>Loading...</h1>
+            )}
+        </div>
 }
 
 export default HomePage;
