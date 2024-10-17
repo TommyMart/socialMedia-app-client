0a7b9c21cd0907650a181a2a6525da37dfb42f9e
Thomas Martin <161907136+TommyMart@users.noreply.github.com>
2024-10-17 15:28:36 +1030

    feat: dynamic id routes implemented.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,15 +3,17 @@ import { Link } from 'react-router-dom';
 import './navBar.css'
 
 function NavBar() {
+
+    const userId = localStorage.getItem('userId');
     
     return (
         <>
         <div className='navBar'>
             <ul>
-                <li><Link to='users/:userId/home'><h3>Home</h3></Link></li>
-                <li><Link to='users/:userId/about'><h3>About</h3></Link></li>
-                <li><Link to='users/:userId/tutorials'><h3>Tutorials</h3></Link></li>
-                <li><Link to='users/:userId/account'><h3>Account</h3></Link></li>
+                <li><Link to={`users/${userId}/home`}><h3>Home</h3></Link></li>
+                <li><Link to={`users/${userId}/about`}><h3>About</h3></Link></li>
+                <li><Link to={`users/${userId}/tutorials`}><h3>Tutorials</h3></Link></li>
+                <li><Link to={`users/${userId}/account`}><h3>Account</h3></Link></li>
             </ul>
         </div>
         </>
